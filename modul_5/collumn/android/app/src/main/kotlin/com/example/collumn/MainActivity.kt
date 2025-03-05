cf246088ba96666f4183e45df670df8aa1b24385
BWBadruzzaman <badruzzaman126@gmail.com>
2025-03-06 05:52:03 +0700

    Modul 5

---
+++ modul_5/collumn/android/app/src/main/kotlin/com/example/collumn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.collumn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ modul_5/nestedrowandcolumn/android/app/src/main/kotlin/com/example/nestedrowandcolumn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nestedrowandcolumn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
